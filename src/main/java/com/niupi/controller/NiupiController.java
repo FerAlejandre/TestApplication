da469f60ae522a2a69f230bceb30e71eb05f20e2
FerAlejandre <FerAlejandre@users.noreply.github.com>
2020-09-22 21:26:38 -0500

    First Commit

---
+++ src/main/java/com/niupi/controller/NiupiController.java
@@ -0,0 +1,31 @@
+package com.niupi.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@RequestMapping(value = "/niupi/")
+@RestController
+@Slf4j
+public class NiupiController {
+
+    @GetMapping(value = "/get")
+    @ResponseStatus(HttpStatus.OK)
+    public String getOperation() {
+        log.info("Calling Get Operation");
+        return "Hello World, this is a get operation";
+    }
+
+    @PostMapping(value = "/post")
+    @ResponseStatus(HttpStatus.OK)
+    public String postOperation() {
+        log.info("Calling Post Operation");
+        return "This is a post";
+    }
+
+
+
+
+}
